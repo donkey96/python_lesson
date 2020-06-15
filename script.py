96542862c169fe227e910be7a6782c8b5f731b40
donkey96 <[noire964@gmail.com]>
2020-06-16 00:59:38 +0900

    先頭・末尾の削除

--- script.py
+++ script.py
@@ -66,3 +66,26 @@ test_str = 'Python-Izm.Com'
 
 print(test_str.upper())
 print(test_str.lower())
+
+# 先頭・末尾の削除
+print('----------------------------------')
+
+test_str = '     python-izm.com'
+print(test_str)
+
+test_str = test_str.lstrip()
+print(test_str)
+
+test_str = test_str.lstrip('python')
+print(test_str)
+
+print('----------------------------------')
+
+test_str = 'python-izm.com     '
+print(test_str + '/')
+
+test_str = test_str.rstrip()
+print(test_str + '/')
+
+test_str = test_str.rstrip('com')
+print(test_str)
