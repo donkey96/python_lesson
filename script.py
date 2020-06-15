325447bf87c0883ff6d6c3e6ab2713474a4ca628
donkey96 <[noire964@gmail.com]>
2020-06-16 00:39:02 +0900

    文字列の連結

--- script.py
+++ script.py
@@ -8,3 +8,20 @@ test_2
 """
 
 print(test_str)
+# 文字列の連結
+test_str = 'python'
+test_str = test_str + '-'
+test_str = test_str + 'izm'
+
+print(test_str)
+
+test_str = '012'
+test_str += '345'
+test_str += '678'
+test_str += '9'
+
+print(test_str)
+
+test_str = '012' * 3
+
+print(test_str)
