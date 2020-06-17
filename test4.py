50cfa40521be9dde79ed21f8c69652303afd4ec5
donkey96 <[noire964@gmail.com]>
2020-06-17 19:02:57 +0900

    要素の削除

--- test4.py
+++ test4.py
@@ -30,12 +30,12 @@ print('YEAR' in test_dict_1)
 print('YEARS' in test_dict_1)
 
 # セット
-test_set_1 = set()
+test_set_1 = {'python', '-', 'izm', '.', 'com'}
 print(test_set_1)
 
 print('-------------------------------------')
 
-test_set_1.add('python')
-test_set_1.update(test_list_1)
+test_set_1.remove('-')
+test_set_1.discard('.')
 
 print(test_set_1)
