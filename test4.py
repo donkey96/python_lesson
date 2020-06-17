b4014a4ad02ccc9e363d83ae0c68d170a39e0ebf
donkey96 <[noire964@gmail.com]>
2020-06-17 18:38:26 +0900

    valueの取得

--- test4.py
+++ test4.py
@@ -26,7 +26,14 @@ print(test_dict_1)
 
 print('=====================================')
 
-for i in test_dict_1:
-  print(i)
-  print(test_dict_1[i])
-  print('------------------------------------------')
+print(test_dict_1['YEAR'])
+
+print('-------------------------------------')
+
+print(test_dict_1.get('YEAR'))
+print(test_dict_1.get('YEARS'))
+
+print('-------------------------------------')
+
+print(test_dict_1.get('YEAR', 'NOT FOUND'))
+print(test_dict_1.get('YEARS', 'NOT FOUND'))
