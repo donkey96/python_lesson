90fdc329dd80837803644294a2cd7297a26c16c9
donkey96 <[noire964@gmail.com]>
2020-06-17 17:36:33 +0900

    日付の計算

--- test3.py
+++ test3.py
@@ -1,27 +1,20 @@
 import datetime
 
-today = datetime.date.today()
-todaydetail = datetime.datetime.today()
+today = datetime.datetime.today()
 
 # 今日の日付
-print('----------------------------------')
 print(today)
-print(todaydetail)
 
-# 今日の日付:それぞれの値
-print('----------------------------------')
-print(today.year)
-print(today.month)
-print(today.day)
-print(todaydetail.year)
-print(todaydetail.month)
-print(todaydetail.day)
-print(todaydetail.hour)
-print(todaydetail.minute)
-print(todaydetail.second)
-print(todaydetail.microsecond)
+# 明日の日付
+print(today + datetime.timedelta(days=1))
 
-# 日付のフォーマット
-print('----------------------------------')
-print(today.isoformat())
-print(todaydetail.strftime("%Y/%m/%d %H:%M:%S"))
+newyear = datetime.datetime(2010, 1, 1)
+
+# 2010年1月1日の一週間後
+print(newyear + datetime.timedelta(days=7))
+
+# 2010年1月1日から今日までの日数
+calc = today - newyear
+
+# 計算結果の戻り値は「timedelta」
+print(calc.days)
