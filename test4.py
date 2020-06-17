392204bffeceb7a0f6d7728844b1fef3d8adf484
donkey96 <[noire964@gmail.com]>
2020-06-17 19:06:43 +0900

    frozenset

--- test4.py
+++ test4.py
@@ -39,3 +39,11 @@ test_set_1.remove('-')
 test_set_1.discard('.')
 
 print(test_set_1)
+
+# frozenset
+test_frozen_1 = frozenset({'python', '-', 'izm', '.', 'com'})
+
+# test_frozen_1.remove('-')
+# test_frozen_1.discard('-')
+
+print(test_frozen_1)
