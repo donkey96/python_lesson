ce2e1175c31c3b8b5e63b3dd30b1f052d4e56715
donkey96 <[noire964@gmail.com]>
2020-06-17 18:09:29 +0900

    要素のインデックスを取得

--- test4.py
+++ test4.py
@@ -16,13 +16,5 @@ print(test_tuple[1])
 print(test_tuple[2])
 
 # リスト
-test_list_1 = ['1', '2', '3', '2', '1']
-print(test_list_1)
-
-print('---------------------------------')
-
-print(test_list_1.pop(1))
-print(test_list_1)
-
-print(test_list_1.pop())
-print(test_list_1)
+test_list_1 = ['100', '200', '300', '200', '100']
+print(test_list_1.index('200'))
