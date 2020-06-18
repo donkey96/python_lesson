12f5d844485e7adf9438633c1a13c38a9315da86
donkey96 <[noire964@gmail.com]>
2020-06-18 20:48:59 +0900

    要素の取得

--- test5.py
+++ test5.py
@@ -1,4 +1,10 @@
 test_list = ['https', 'www', 'python', 'izm', 'com']
 
-print(test_list[:])
-print(test_list[::])
+print(test_list[:4])
+print(test_list[2:])
+print(test_list[::2])
+print(test_list[3:5])
+print(test_list[-1:])
+print(test_list[:-1])
+print(test_list[::-1])
+print(test_list[:999])
