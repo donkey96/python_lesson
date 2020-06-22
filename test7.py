699cc9070951b7ec59755486444cd57ea9ae631f
donkey96 <[noire964@gmail.com]>
2020-06-22 17:28:03 +0900

    複数の値に分割して取得

--- test7.py
+++ test7.py
@@ -9,3 +9,11 @@ for_sample.append("sample")
 
 for value in 'ABCDEF':
   print(value)
+
+test_list_1 =[['https', 'www'], ['python-izm', 'com']]
+
+for value in test_list_1:
+  print(value)
+
+for value_1, value_2 in test_list_1:
+  print(value_1, value_2)
