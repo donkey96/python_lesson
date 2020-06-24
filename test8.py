8c2755ca9684c3c377c35e5a4937264b749876e8
donkey96 <[noire964@gmail.com]>
2020-06-24 22:54:17 +0900

    フォーマット出力

--- test8.py
+++ test8.py
@@ -7,3 +7,17 @@ print('com')
 f_obj = open('test.txt', 'w')
 
 print('python-izm.com', file=f_obj)
+
+# フォーマット出力
+print('Pythonの学習サイト : %s' % 'python-izm.com')
+print('Pythonの学習サイト : %s-%s.%s' % ('python', 'izm', 'com'))
+
+test_int = 100 + 100
+test_str = 'python-izm.com'
+print('サイト開設 %d 日目! %s' % (test_int, test_str))
+
+print('Pythonの学習サイト : {}'.format('python-izm.com'))
+print('Pythonの学習サイト : {0}-{1}.{2}'.format('python', 'izm', 'com'))
+
+print('サイト開設 {1} 日目! {0}'.format(test_str, test_int))
+
