0371622a04f9434813ee5da614a69c2a44bafc27
donkey96 <[noire964@gmail.com]>
2020-06-26 05:45:18 +0900

    引数の初期値

--- test9.py
+++ test9.py
@@ -1,4 +1,4 @@
-def test_func(num_1, num_2, oprn):
+def test_func(num_1, num_2, oprn=1):
 
   if oprn == 1:
     print('足し算開始')
@@ -19,4 +19,5 @@ def test_func(num_1, num_2, oprn):
   else:
     print('不明なオペレーション指定です')
 
-test_func(100, 10, 3)
+test_func(100, 10)
+test_func(100, 10, 4)
