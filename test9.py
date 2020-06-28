c8acc47f3a520356fa4dba15d948a6226e0b9724
donkey96 <[noire964@gmail.com]>
2020-06-28 14:10:36 +0900

    インスタンスメソッド

--- test9.py
+++ test9.py
@@ -15,9 +15,19 @@ new.test_method('method call')
 # メソッドの種類
 class TestClass:
 
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
   # インスタンスメソッド
-  def sample_instancemethod(self, arg_1):
-    pass
+  def sample_instancemethod(self, display_x=True, display_y=True):
+    if display_x:
+      print('x is {}'.format(self.x))
+    if display_y:
+      print('y is {}'.format()(self.y))
+
+  test_class_1 = TestClass(100, 50)
+  test_class_1.sample_instancemethod(display_x=False)
   # クラスメソッド
   @classmethod
   def sample_classmethod(cls, arg_1):
