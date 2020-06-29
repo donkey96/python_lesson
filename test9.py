f39985549af519917f2aeb9690fc5e25d01ca1e7
donkey96 <[noire964@gmail.com]>
2020-06-29 17:45:33 +0900

    クラスメソッド

--- test9.py
+++ test9.py
@@ -13,26 +13,34 @@ new = NewStyleClass()
 new.test_method('method call')
 
 # メソッドの種類
+import datetime
+
 class TestClass:
 
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
-
-  # インスタンスメソッド
-  def sample_instancemethod(self, display_x=True, display_y=True):
-    if display_x:
-      print('x is {}'.format(self.x))
-    if display_y:
-      print('y is {}'.format()(self.y))
-
-  test_class_1 = TestClass(100, 50)
-  test_class_1.sample_instancemethod(display_x=False)
+  def __init__(self, year, month, day):
+    self.year = year
+    self.month = month
+    self.day = day
   # クラスメソッド
   @classmethod
-  def sample_classmethod(cls, arg_1):
-    pass
-  # スタティックメソッド
-  @staticmethod
-  def sample_staticmethod(arg_1, arg_2):
-    pass
+  def sample_classmethod(cls, date_diff=0):
+    today = datetime.date.today()
+    d = today + datetime.timedelta(days=date_diff)
+    return cls(d.year, d.month, d.day)
+
+# インスタンス化しないで呼び出し
+test_class_1 = TestClass.sample_classmethod()
+print(test_class_1.year, test_class_1.month, test_class_1.day)
+
+# インスタンス化しないで呼び出し
+test_class_2 = TestClass.sample_classmethod(-10)
+print(test_class_2.year, test_class_2.month, test_class_2.day)
+
+# 通常のインスタンス
+test_class_3 = TestClass(2000, 1, 1)
+print(test_class_3.year, test_class_3.month, test_class_3.day)
+
+  # # スタティックメソッド
+  # @staticmethod
+  # def sample_staticmethod(arg_1, arg_2):
+  #   pass
