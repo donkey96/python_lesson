e586255262bafaa247d43cab57e9b51215b8a152
donkey96 <[noire964@gmail.com]>
2020-07-03 21:28:01 +0900

    デコレータを使用する

--- test1.py
+++ test1.py
@@ -19,3 +19,14 @@ class ContextManagerTest:
 
 with ContextManagerTest() as as_obj:
   print(as_obj)
+
+from contextlib import contextmanager
+
+@contextmanager
+def context_manager_test():
+  print('enter')
+  yield 'as obj'
+  print('exit')
+
+with context_manager_test() as as_obj:
+  print(as_obj)
