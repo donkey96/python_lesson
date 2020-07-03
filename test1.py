2e169d5ee43b539b153b4049204f550b9ba3cc99
donkey96 <[noire964@gmail.com]>
2020-07-03 21:21:04 +0900

    特殊メソッドを実装する

--- test1.py
+++ test1.py
@@ -7,3 +7,15 @@ with open('read.txt') as f:
   print(f.closed)
 
 print(f.closed)
+
+class ContextManagerTest:
+  
+  def __enter__(self):
+    print('__enter__')
+    return 'as obj'
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    print('__exit__')
+
+with ContextManagerTest() as as_obj:
+  print(as_obj)
