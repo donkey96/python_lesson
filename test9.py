43cb3001d201188903080f14a2fc9bad4ee60f08
donkey96 <[noire964@gmail.com]>
2020-07-03 20:18:46 +0900

    ファイルの読み込み

--- test9.py
+++ test9.py
@@ -65,3 +65,11 @@ gen_sample = (i for i in 'おはよう こんにちは こんばんは'.split())
 print(gen_sample)
 for i in gen_sample:
   print(i)
+
+# ファイルの読み込み
+f = open('read.txt', 'r')
+
+for row in f:
+  print(row)
+
+f.close()
