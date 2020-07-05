84cf11d99aaa40d1bf2a3fb59740a2624c3d5939
donkey96 <[noire964@gmail.com]>
2020-07-06 03:48:33 +0900

    getter内で他の属性を参照

--- test1.py
+++ test1.py
@@ -1,23 +1,14 @@
 class PropertyTest(object):
 
-  def __init__(self, url):
-    self._url = url
+  def __init__(self, scheme, host):
+    self.schema = scheme
+    self.host = host
 
   def get_url(self):
-    print('-- get_url --')
-    return self._url
+    return('{}://{}/'.format(self.schema, self.host))
 
-  def set_url(self, url):
-    print('-- set_url --')
-    self._url = url
+  url = property(get_url)
 
-  def del_url(self):
-    del self._url
-
-  url = property(get_url, set_url, del_url, 'url Property')
-
-prop = PropertyTest('https://www.python-izm.com/')
-
-prop.url = 'python-izm'
+prop = PropertyTest('https', 'www.python-izm.com/')
 
 print(prop.url)
