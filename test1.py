113a19f435946c4e2c3920aa37727f413cf85566
donkey96 <[noire964@gmail.com]>
2020-07-10 13:55:10 +0900

    zip_longest関数

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
-item_list = ['desktop', 'laptop', 'tablet', 'smartphone']
-stock_list = [12, 83, 55, 0]
+from itertools import zip_longest
 
-for item_name, stock_count in zip(item_list, stock_list):
+item_list = ['desktop', 'laptop', 'tablet', 'smartphone']
+stock_list = [12, 83, 55]
+
+for item_name, stock_count in zip_longest(item_list, stock_list, fillvalue='no stock'):
   print('{} / {}'.format(item_name, stock_count))
