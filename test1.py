6f47372c21ad514cc6cf260c60bf562576ea73e0
donkey96 <[noire964@gmail.com]>
2020-07-13 18:45:57 +0900

    別の記述法

--- test1.py
+++ test1.py
@@ -8,7 +8,15 @@ print('-------------------------------')
 print(test_set_1.issubset({'python', 'izm'}))
 print(test_set_1.issubset({'www', 'python', '-', 'izm', '.', 'com'}))
 
+# issubsetと同じ
+print(test_set_1 <= {'python', 'izm'})
+print(test_set_1 <= {'www', 'python', '-', 'izm', '.', 'com'})
+
 print('-------------------------------')
 
 print(test_set_1.issuperset({'python', 'izm'}))
 print(test_set_1.issuperset({'www', 'python', '-', 'izm', '.', 'com'}))
+
+# issupersetと同じ
+print(test_set_1 >= {'python', 'izm'})
+print(test_set_1 >= {'www', 'python', '-', 'izm', '.', 'com'})
