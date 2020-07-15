e142f610b617e5a390abfd8a87fae55941433e38
donkey96 <[noire964@gmail.com]>
2020-07-15 13:50:41 +0900

    変数の型チェック

--- test1.py
+++ test1.py
@@ -1,11 +1,41 @@
-comp_list = [i for i in range(10) if i % 2 == 1]
+class TestClass:
 
-print(comp_list)
+  def __init__(self, title):
+    self.title = title
 
-comp_dict = {str(i): i * i for i in range(10)}
+tuple_instance = ('tuple', 'instance')
+list_instance = ['list', 'instance']
+dict_instance = {'1':'dict', '2':'instance'}
+class_instance = TestClass('class')
 
-print(comp_dict)
+python_list = []
+python_list.append('python')
+python_list.append('izm')
+python_list.append(tuple_instance)
+python_list.append(list_instance)
+python_list.append(dict_instance)
+python_list.append(class_instance)
 
-comp_set = {str(i * i) for i in range(10)}
+for parent in python_list:
+  if isinstance(parent, tuple):
+    print('+++ tuple +++')
+    for child in parent:
+      print(child)
 
-print(comp_set)
+  elif isinstance(parent, list):
+    print('+++ list +++')
+    for child in parent:
+      print(child)
+
+  elif isinstance(parent, dict):
+    print('+++ dict +++')
+    for child in parent.keys():
+      print(parent[child])
+
+  elif isinstance(parent, TestClass):
+    print('+++ TestClass +++')
+    print(parent.title)
+   
+  else:
+    print('-- string --')
+    print(parent)
