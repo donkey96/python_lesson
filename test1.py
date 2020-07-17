3f24022f108736c0ca243b6de719bf14d85a954b
donkey96 <[noire964@gmail.com]>
2020-07-17 18:19:36 +0900

    getattr

--- test1.py
+++ test1.py
@@ -6,6 +6,6 @@ class AttrTest():
 attr_test = AttrTest()
 attr_test.name = 'python-izm'
 
-print(hasattr(attr_test, 'code'))
-print(hasattr(attr_test, 'name'))
-print(hasattr(attr_test, 'kana'))
+print(getattr(attr_test, 'code'))
+print(getattr(attr_test, 'name'))
+print(getattr(attr_test, 'kana', 'No Attr'))
