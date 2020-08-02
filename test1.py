bfd4bd0537f1c7e6b04ed5f8e5a3625390f041a2
donkey96 <[noire964@gmail.com]>
2020-08-03 04:04:39 +0900

    csvファイルの読み込み

--- test1.py
+++ test1.py
@@ -1,23 +1,11 @@
 import csv
 
-class CustomFormat(csv.excel):
-  quoting = csv.QUOTE_ALL
+csv_file = open('./python.csv', 'r', newline='')
+reader = csv.reader(csv_file)
 
-csv.register_dialect('myformat', CustomFormat)
-
-csv_file = open('./python.csv', 'w', newline='')
-writer = csv.writer(
-  csv_file,
-  dialect='myformat',
-  )
-
-row = ('python', '-', 'izm', '1')
-writer.writerow(row)
-
-rows = []
-rows.append(('python', '-', 'izm', '2'))
-rows.append(('python', '-', 'izm', '3'))
-rows.append(('p,y,t,h,o,n', '-', 'i,z,m', '4'))
-writer.writerows(rows)
-
+for row in reader:
+  print('-----------------------')
+  for cell in row:
+    print(cell)
+    
 csv_file.close()
