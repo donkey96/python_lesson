dba632f4fe70202678daa5b907945633848ac6c4
donkey96 <[noire964@gmail.com]>
2020-08-15 23:18:42 +0900

    HTML文字列の取得

--- test1.py
+++ test1.py
@@ -1,22 +1,8 @@
-import smtplib
+import urllib.request
 
-from email.mime.text import MIMEText
-from email.header import Header
-from email.utils import formatdate
+url = 'http://www.python-izm.com/'
 
-from_address = 'from@xxx.xx.jp'
-to_address = 'from@xxx.xx.jp'
+htmldata = urllib.request.urlopen(url)
+print(htmldata.read().decode('UTF-8'))
 
-charset = 'ISO-2022-JP'
-subject = 'メールの件名です'
-text = 'メールの本文です'
-
-msg = MIMEText(text, 'plain', charset)
-msg['Subject'] = Header(subject, charset)
-msg['From'] = from_address
-msg['To'] = to_address
-msg['Date'] = formatdate(localtime=True)
-
-smtp = smtplib.SMTP('xxx.xx.jp')
-smtp.sendmail(from_address, to_address, msg.as_string())
-smtp.close()
+htmldata.close()
