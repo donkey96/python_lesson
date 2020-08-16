8bbc557da6c6161c6b1a7952fec9feed6a829a3a
donkey96 <[noire964@gmail.com]>
2020-08-16 19:13:05 +0900

    ヘッダ情報の設定

--- test1.py
+++ test1.py
@@ -2,7 +2,16 @@ import urllib.request
 
 url = 'http://www.python-izm.com/'
 
-htmldata = urllib.request.urlopen(url)
+opener = urllib.request.build_opener()
+opener.addheaders = [
+  (
+    'User-agent',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 ( .NET CLR 3.5.30729)'
+  )
+]
+
+htmldata = opener.open(url)
 print(htmldata.read().decode('UTF-8'))
 
 htmldata.close()
+opener.close()
