93205b7dd8178d6b69d41eb0c2ee2638f96f9518
donkey96 <[noire964@gmail.com]>
2020-08-21 19:02:37 +0900

    JSON文字列からオブジェクト

--- test1.py
+++ test1.py
@@ -1,10 +1,11 @@
 import json
 
-json_data = {'PythonEngine':('google.co.jp', 'yahoo.co.jp')}
+json_data = {'Python':'python-izm.com',
+             'PythonEngine':('google.co.jp', 'yahoo.co.jp')}
 
-print(type(json_data))
-
-encode_json_data = json.dumps(json_data, indent=4)
-
-print(encode_json_data)
+encode_json_data = json.dumps(json_data)
 print(type(encode_json_data))
+
+decode_json_data = json.loads(encode_json_data)
+print(decode_json_data)
+print(type(decode_json_data))
