aebdb144e55e939d4e071fda775fb501e1dcc85f
donkey96 <[noire964@gmail.com]>
2020-08-21 18:57:34 +0900

    見やすい形に

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ json_data = {'PythonEngine':('google.co.jp', 'yahoo.co.jp')}
 
 print(type(json_data))
 
-encode_json_data = json.dumps(json_data)
+encode_json_data = json.dumps(json_data, indent=4)
 
 print(encode_json_data)
 print(type(encode_json_data))
