499704501b75f5ab20fdf4dd22f20e5e5a69978d
donkey96 <[noire964@gmail.com]>
2020-09-16 23:33:18 +0900

    型の変更

--- test1.py
+++ test1.py
@@ -6,4 +6,18 @@ str_1 = "He"
 str_2 = "llo"
 aisatsu = str_1 + str_2
 aisatsu += " World"
+
 print(aisatsu)
+
+val = 100
+str_val = str(val)
+cm = "センチ"
+len = str_val + cm
+
+print(len)
+
+int_val = int(str_val)
+float_val = float(str_val)
+
+print(int_val)
+print(float_val)
