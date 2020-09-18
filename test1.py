548f251e5968efdb2fa66fb6e61d1acf10cb1dab
donkey96 <[noire964@gmail.com]>
2020-09-18 18:29:40 +0900

    加減算

--- test1.py
+++ test1.py
@@ -1,23 +1,3 @@
-val = 2 * (3 + 4) * 1
+a = 4 + 3
+hello = "Hello" + "World"
 
-print(val)
-
-str_1 = "He"
-str_2 = "llo"
-aisatsu = str_1 + str_2
-aisatsu += " World"
-
-print(aisatsu)
-
-val = 100
-str_val = str(val)
-cm = "センチ"
-len = str_val + cm
-
-print(len)
-
-int_val = int(str_val)
-float_val = float(str_val)
-
-print(int_val)
-print(float_val)
