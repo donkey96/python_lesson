7b0b9b60e1649b7701598dd8e9736e57203c5dbb
donkey96 <[noire964@gmail.com]>
2020-09-22 18:01:01 +0900

    リストの連結

--- test1.py
+++ test1.py
@@ -1,6 +1,9 @@
 color = ["white", "black", "red", "blue", "yellow"]
+color2 = ["green", "brown", "purple", "pink"]
 
-pic = color[0]
+pic = color[0] + " and " + color[4]
+total_color = color + color2
 
 print(color[1])
 print(pic)
+print(total_color)
