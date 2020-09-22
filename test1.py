c787e06249befed1390c8eb2d865b9c1b884b44e
donkey96 <[noire964@gmail.com]>
2020-09-22 18:02:12 +0900

    置き換え

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 color = ["white", "black", "red", "blue", "yellow"]
 color2 = ["green", "brown", "purple", "pink"]
+color[4] = "gray"
 
 pic = color[0] + " and " + color[4]
 total_color = color + color2
