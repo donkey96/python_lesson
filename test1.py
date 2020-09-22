6109e4a99eda2255a9661e22830cf1ad1ce2519f
donkey96 <[noire964@gmail.com]>
2020-09-22 18:08:02 +0900

    slice

--- test1.py
+++ test1.py
@@ -5,7 +5,13 @@ del color[0]
 
 pic = color[0] + " and " + color[3]
 total_color = color + color2
+slice = color[1:3]
+slice2 = color[:2]
+slice3 = color[1:]
 
 print(color[1])
 print(pic)
 print(total_color)
+print(slice)
+print(slice2)
+print(slice3)
