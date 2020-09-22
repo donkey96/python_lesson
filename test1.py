51cb62723f5459d057866497a38d545504fb968b
donkey96 <[noire964@gmail.com]>
2020-09-22 18:03:53 +0900

    要素の削除

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
 color = ["white", "black", "red", "blue", "yellow"]
 color2 = ["green", "brown", "purple", "pink"]
 color[4] = "gray"
+del color[0]
 
-pic = color[0] + " and " + color[4]
+pic = color[0] + " and " + color[3]
 total_color = color + color2
 
 print(color[1])
