5c0d3052400c5f927cc1b6f59861f384a26a0514
donkey96 <[noire964@gmail.com]>
2020-09-24 18:25:12 +0900

    タプル

--- test1.py
+++ test1.py
@@ -1,17 +1,9 @@
-color = ["white", "black", "red", "blue", "yellow"]
-color2 = ["green", "brown", "purple", "pink"]
-color[4] = "gray"
-del color[0]
+prefectures = ("Tokyo", "Osaka", "Kyoto", "Nagoya", "Fukuoka", "Hokkaido")
+trip = prefectures[0]
+shikoku = ("Tokushima", "Ehime", "Kochi", "Kagawa")
 
-pic = color[0] + " and " + color[3]
-total_color = color + color2
-slice = color[1:3]
-slice2 = color[:2]
-slice3 = color[1:]
+prefectures += shikoku
 
-print(color[1])
-print(pic)
-print(total_color)
-print(slice)
-print(slice2)
-print(slice3)
+print(prefectures[1])
+print(trip)
+print(prefectures)
