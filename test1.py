b01b45b175c2e237474918e8903f315b31cb0e96
donkey96 <[noire964@gmail.com]>
2020-10-09 08:21:31 +0900

    3の倍数かつ5の倍数の時FizzBuzzになるよう条件分岐

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
 for count in range(1,101):
-  if count % 3 == 0:
+  if count % 3 == 0 and count % 5 == 0:
+    print("FizzBuzz")
+  elif count % 3 == 0:
     print("Fizz")
   elif count % 5 == 0:
     print("Buzz")
