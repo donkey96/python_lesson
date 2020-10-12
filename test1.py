50ebc614f85831648b764ce1f4629d307451a14b
donkey96 <[noire964@gmail.com]>
2020-10-13 06:23:11 +0900

    リストの使い方

--- test1.py
+++ test1.py
@@ -1,3 +1,21 @@
-eigo = ['a', 'b', 'c']
+eigo = ['a', 'b', 'c', 'd', 'e', 'f']
 
-eigo[0]
+char = eigo[2]
+
+print(char)
+
+char = eigo[-1]
+
+print(char)
+
+chars = eigo[1:4]
+
+print(chars)
+
+chars = eigo[3:]
+
+print(chars)
+
+chars = eigo[:4]
+
+print(chars)
