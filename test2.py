a8ec05eed45cf9511c4f9075c27fef8e83effe8e
donkey96 <[noire964@gmail.com]>
2020-12-06 22:14:08 +0900

    多次元のリストで応用

--- test2.py
+++ test2.py
@@ -10,3 +10,7 @@ strheight = "170,168,180,17x,165"
 height = [int(x) for x in strheight.split(',') if x.isdigit()]
 
 print(height)
+
+data = [[x+y*3 for x in range(3)] for y in range(4)]
+
+print(data)
