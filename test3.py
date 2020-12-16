c236104c1c98be9f92d2867b5cfb8a82dccda0a6
donkey96 <[noire964@gmail.com]>
2020-12-16 20:34:21 +0900

    位置引数

--- test3.py
+++ test3.py
@@ -5,3 +5,11 @@ def func(**keywords):
   for kw in keywords:
     print(kw, ":", keywords[kw])
 func(a=1, b=2, c=3, d=4)
+
+# func(3, 5)
+# func(*(3, 5))
+
+def func(*arguments):
+  for arg in arguments:
+    print(arg)
+func(1, 2, 3, 4)
